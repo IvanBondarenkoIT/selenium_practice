c6c56b892560675bf6612b0cae6c82540ef75ac5
Ivan Bondarenko <krenkroyt@gmail.com>
2024-01-18 16:15:48 +0400

    find element by NAME, ID, CSS_SELECTOR

--- main.py
+++ main.py
@@ -7,13 +7,26 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get(
-    "https://www.amazon.com/KOORUI-Inch-Computer-Monitor-Ultra-Thin/dp/B0C4DNVG5H?pf_rd_r=SY8QBX51ANWMPE7J87YY&pf_rd_t=Events&pf_rd_i=deals&pf_rd_p=2364d0f5-5054-4700-a4e8-74a53fead388&pf_rd_s=slot-14&ref=dlx_deals_gd_dcl_img_6_46db9c7a_dt_sl14_88"
-)
+# driver.get(
+#     "https://www.amazon.com/KOORUI-Inch-Computer-Monitor-Ultra-Thin/dp/B0C4DNVG5H?pf_rd_r=SY8QBX51ANWMPE7J87YY&pf_rd_t=Events&pf_rd_i=deals&pf_rd_p=2364d0f5-5054-4700-a4e8-74a53fead388&pf_rd_s=slot-14&ref=dlx_deals_gd_dcl_img_6_46db9c7a_dt_sl14_88"
+# )
+#
+# price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+# price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+# print(f"${price_dollar.text}.{price_cents.text}")
 
-price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
-price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
-print(f"${price_dollar.text}.{price_cents.text}")
+
+driver.get("https://www.python.org/")
+
+search_bar = driver.find_element(By.NAME, value="q")
+print(search_bar.get_attribute("placeholder"))
+print(search_bar.tag_name)
+button = driver.find_element(By.ID, value="submit")
+print(button.size)
+documentation_link = driver.find_element(
+    By.CSS_SELECTOR, value=".documentation-widget a"
+)
+print(documentation_link.text)
 
 
 # driver.close()
