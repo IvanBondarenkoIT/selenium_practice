eadc49649d4815ccfbb4d5e6e7ba30a47558ebff
Ivan Bondarenko <krenkroyt@gmail.com>
2024-01-18 19:28:45 +0400

    Sending keyboard input to Selenium

--- interaction.py
+++ interaction.py
@@ -10,13 +10,20 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-article_count = driver.find_element(
-    By.XPATH,
-    "/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/div[1]/div/div[3]/a[1]",
-)
+# article_count = driver.find_element(
+#     By.XPATH,
+#     "/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/div[1]/div/div[3]/a[1]",
+# )
+#
+# article_count.click()
+# print(article_count.text)
 
-article_count.click()
+# all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
 
-print(article_count.text)
+# Find the "Search <input> by Name
+search = driver.find_element(By.NAME, value="search")
+# Sending keyboard input to Selenium
+search.send_keys("Python")
 
 # driver.quit()
